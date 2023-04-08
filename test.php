3fb4daf0148e4d42149cf47a391c5546db0929e7
SABARISH <128790623+sabarishkarmegamk@users.noreply.github.com>
2023-04-08 17:22:16 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,14 +1,15 @@
 
+        
         <?php
 
     // Connect to database
     $con = mysqli_connect("localhost","root","","test");
 
         if(isset($_POST['submit'])){
-            $three=$_POST['three'];
+            $price=$_POST['price'];
             $tax =$_POST['tax'];
            
-            $selling = $three  * ((100 + $tax) / 100);
+            $selling = $price  * ((100 + $tax) / 100);
             echo $selling;
             
             $sql = "INSERT INTO maths (tax) VALUES ($selling)";
@@ -28,11 +29,8 @@
     <form method="post">
     <h4><b>Enter 1st Value</b></h4>
     <h1>Price:</h1>
-           <input type="text" name="three" id="three" class="form-control" placeholder="">
+           <input type="text" name="price" id="price" class="form-control" placeholder="price">
             <br>
-            <h1>tax Rate</h1><input type="text" name="tax" id="tax" class="form-control" placeholder="">
+            <h1>tax Rate</h1><input type="text" name="tax" id="tax" class="form-control" placeholder="tax rate">
        <input type="submit" name="submit">
     </form>
-      
-      <h4><b>Result is:</b></h4>  
-      <br >
